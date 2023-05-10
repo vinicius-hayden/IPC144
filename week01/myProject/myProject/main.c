df9c7f1d3f0d9a26bbb2bab6ae057c4329232d37
vinicius-hayden <vinihaydenss@gmail.com>
2023-05-10 11:16:21 -0400

    Hello World

---
+++ week01/myProject/myProject/main.c
@@ -0,0 +1,14 @@
+/*
+ 
+* Name: Vinicius H S Silva
+* Description: My Project
+ 
+*/
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Welcome to C!!!!!\n\n\n");
+    printf("My name is Vinicius");
+    return 0;
+}
